7a768c7ce7405bad08ff8481e93e94b1d2be3425
bboysquiz <42135241+bboysquiz@users.noreply.github.com>
2018-08-06 11:36:16 +0300

    dbvs

--- index.php
+++ index.php
@@ -12,14 +12,8 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/icomoon-social.css">
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600,800' rel='stylesheet' type='text/css'>
 
-        <link rel="stylesheet" href="css/leaflet.css" />
-		<link rel="stylesheet" href="css/main.css">
-
-        <script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+      
     </head>
     <body>
         <?php if( isset($_SESSION['logged_user']) ) :?>
@@ -251,14 +245,8 @@
 
         <!-- Javascripts -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/jquery-1.9.1.min.js"><\/script>')</script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.js"></script>
-        <script src="js/jquery.fitvids.js"></script>
-        <script src="js/jquery.sequence-min.js"></script>
-        <script src="js/jquery.bxslider.js"></script>
-        <script src="js/main-menu.js"></script>
-        <script src="js/template.js"></script>
+       
+  
 
     </body>
 </html>
